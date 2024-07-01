78a1e3b00ae64ea069423bdc5ccfef8299ec1dc7
King-Rian <kingrianlaurence1302@gmail.com>
2024-07-01 13:38:25 -0400

    Lines

--- savings_account.py
+++ savings_account.py
@@ -17,10 +17,10 @@ def create_savings_account(balance, interest_rate, months):
     # Create an instance of the `Account` class and pass in the balance and interest parameters.
     #  Hint: You need to add the interest as a value, i.e, 0.
     # ADD YOUR CODE HERE
-
+    account = Account(balance, 0) 
     # Calculate interest earned
      # ADD YOUR CODE HERE
-
+    interest_earned = balance * (interest_rate / 100) * (months / 12)
     # Update the savings account balance by adding the interest earned
     # ADD YOUR CODE HERE
 
